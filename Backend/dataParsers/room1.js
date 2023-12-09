bd3d8cef5152a16102a35513fcd0840f351b4994
anas12312 <anashesham000@gmail.com>
2023-12-09 13:41:04 +0200

    anas

---
+++ Backend/dataParsers/room1.js
@@ -0,0 +1,31 @@
+function dataParserRoom1(data) {
+    const parsedData = {
+
+        BSSID1: data.data[0]? data.data[0].BSSID : undefined,
+        BSSID2: data.data[1]? data.data[1].BSSID : undefined,
+        BSSID3: data.data[2]? data.data[2].BSSID : undefined,
+        BSSID4: data.data[3]? data.data[3].BSSID : undefined,
+        BSSID5: data.data[4]? data.data[4].BSSID : undefined,
+
+        RSSI1: data.data[0]? data.data[0].level : undefined,
+        RSSI2: data.data[1]? data.data[1].level : undefined,
+        RSSI3: data.data[2]? data.data[2].level : undefined,
+        RSSI4: data.data[3]? data.data[3].level : undefined,
+        RSSI5: data.data[4]? data.data[4].level : undefined,
+
+        Frequency1: data.data[0]? data.data[0].level : undefined,
+        Frequency2: data.data[1]? data.data[1].level : undefined,
+        Frequency3: data.data[2]? data.data[2].level : undefined,
+        Frequency4: data.data[3]? data.data[3].level : undefined,
+        Frequency5: data.data[4]? data.data[4].level : undefined,
+    
+        accelerometer: data.accelerometerSensor,
+        magnetometer: data.magnetometerSensor,
+
+        location: data.roomNum == 1? 'indoor' : 'outdoor'
+
+    }
+    return parsedData
+}
+
+module.exports = dataParserRoom1
--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,23 @@
 const express = require('express')
+const dataParserRoom1 = require('./dataParsers/room1')
+const dataParserRoom2 = require('./dataParsers/room2')
+const dataParserRoom3 = require('./dataParsers/room3')
+const append = require('./append')
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
 
-app.post('/room1', (req, res) => {
-    console.log(req.body)
+app.post('/read', (req, res) => {
+    append(dataParserRoom1(req.body), 1)
+    append(dataParserRoom2(req.body), 2)
+    append(dataParserRoom3(req.body), 3)
+    res.send({
+        message: req.body.roomNum == 0? 'Data appended as outdoor in every room' : 'Data appended as indoor in room: ' + req.body.roomNum
+    })
 })
 
-app.post('/room2', (req, res) => {
-
-})
-
-app.post('/room3', (req, res) => {
-
-})
-
-app.post('/out', (req, res) => {
-
-})
 
 app.listen(port, () => {
     console.log("Rawan is running on port: " + port)
